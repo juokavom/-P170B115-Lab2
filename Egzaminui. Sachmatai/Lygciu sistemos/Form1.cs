b7d9fd869bf4c263616e0151f07e2c90ab39f915
Jokūbas Akramas <j.akramas@gmail.com>
2020-12-20 22:48:38 +0200

    Pradinis rikio vaiksciojimas

--- Egzaminui. Sachmatai/Lygciu sistemos/Form1.cs
+++ Egzaminui. Sachmatai/Lygciu sistemos/Form1.cs
@@ -3,6 +3,7 @@
 //Egzamino užduotis
 
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -10,8 +11,54 @@ namespace Pvz1
 {
     public partial class Form1 : Form
     {
-        static int[] BK = {0, 4};
-        static int[] XY = { 8, 8};
+        private abstract class Figure
+        {
+            public int X;
+            public int Y;
+            public abstract void Move();
+        }
+        private class Rook : Figure
+        {
+            public Rook(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public override void Move()
+            {
+                List<int> list = new List<int>();
+                if (X != 7) list.Add(6);
+                if (X != 0) list.Add(4);
+                if (Y != 7) list.Add(8);
+                if (Y != 0) list.Add(2);
+                Random rnd = new Random();
+                int move = rnd.Next(0, list.Count);
+                System.Diagnostics.Debug.WriteLine(string.Format("Rnd:{0}, list.Count: {1}, list[rnd]: {2}.", move, list.Count, list[move]));
+                switch (list[move])
+                {
+                    case 6:
+                        int length = 7 - X;
+                        int step = rnd.Next(1, length);
+                        X += step;
+                        break;
+                    case 4:
+                        length = X;
+                        step = rnd.Next(1, length);
+                        X -= step;
+                        break;
+                    case 8:
+                        length = 7 - Y;
+                        step = rnd.Next(1, length);
+                        Y += step;
+                        break;
+                    case 2:
+                        length = Y;
+                        step = rnd.Next(1, length);
+                        Y -= step;
+                        break;
+                }
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -38,13 +85,61 @@ namespace Pvz1
             //---
             //---
         }
-        private void p(string text)
+        private void fillValues(int[,] A)
         {
-            System.Diagnostics.Debug.WriteLine(text);
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int u = 0; u < A.GetLength(1); u++)
+                {
+                    A[i, u] = 0;
+                }
+            }
+
+        } 
+        private void printMatrix(int[,] A)
+        {
+            for (int i = A.GetLength(1) - 1; i >= 0 ; i--)
+            {
+                for (int u = 0; u < A.GetLength(0); u++)
+                {
+                    Write(A[u, i].ToString());
+                }
+                WriteLine("");
+            }
+
         }
-        private void Solution() 
+        private void Write(string text)
         {
-            p("Hello, world!");
+            System.Diagnostics.Debug.Write(text);
+        }
+        private void WriteLine(string text)
+        {
+            Write(text + "\n");
+        }
+       
+        private void Solution()
+        {
+            int[,] A = new int[8, 8];
+            fillValues(A);
+            Rook rook = new Rook(7, 7);
+
+            A[4, 0] = 8;
+            A[4, 7] = 1;
+            A[5, 7] = 3;
+            A[7, 7] = 2;
+            A[1, 7] = 4;
+
+            printMatrix(A);
+
+            for (int i = 0; i < 10; i++)
+            {
+                A[rook.X, rook.Y] = 0;
+                rook.Move();
+                A[rook.X, rook.Y] = 2;
+                printMatrix(A);
+                WriteLine("");
+            }
+
         }
         private void button1_Click(object sender, EventArgs e)
         {
