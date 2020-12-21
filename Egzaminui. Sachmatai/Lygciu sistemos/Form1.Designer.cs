d8d0504f7ca68b0df2a8c37154db863e770114da
Jokūbas Akramas <j.akramas@gmail.com>
2020-12-21 02:28:35 +0200

    Rodomas figūrų padengimas su checkbox ir dinaminis ėjimas mygtuku

--- Egzaminui. Sachmatai/Lygciu sistemos/Form1.Designer.cs
+++ Egzaminui. Sachmatai/Lygciu sistemos/Form1.Designer.cs
@@ -38,45 +38,33 @@ namespace Pvz1
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(20, 13);
-            this.chart1.Margin = new System.Windows.Forms.Padding(4);
-            this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(400, 400);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
             // 
             // richTextBox1
             // 
             this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.richTextBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.richTextBox1.Location = new System.Drawing.Point(20, 621);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 528);
             this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(886, 61);
+            this.richTextBox1.Size = new System.Drawing.Size(829, 137);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(1063, 731);
+            this.button1.Location = new System.Drawing.Point(997, 714);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(210, 52);
@@ -87,19 +75,19 @@ namespace Pvz1
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(698, 22);
+            this.button2.Location = new System.Drawing.Point(605, 43);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(208, 31);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Užduotis";
+            this.button2.Text = "Kitas ėjimas";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
             this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button4.Location = new System.Drawing.Point(1175, 731);
+            this.button4.Location = new System.Drawing.Point(1109, 714);
             this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 28);
@@ -108,11 +96,36 @@ namespace Pvz1
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(20, 20);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(550, 500);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(605, 101);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(144, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Rodyti padengimą";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(921, 695);
+            this.ClientSize = new System.Drawing.Size(855, 678);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -123,6 +136,7 @@ namespace Pvz1
             this.Text = "SMA EGZAMINAS Jokūbas Akramas";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -135,11 +149,12 @@ namespace Pvz1
 
         }
         #endregion
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private Button button4;
+        private Chart chart1;
+        private CheckBox checkBox1;
     }
 
 
--- Egzaminui. Sachmatai/Lygciu sistemos/Form1.cs
+++ Egzaminui. Sachmatai/Lygciu sistemos/Form1.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -12,20 +13,36 @@ namespace Pvz1
     public partial class Form1 : Form
     {
         private static int[,] A = new int[8, 8];
+        private static PictureBox[,] pbErr = new PictureBox[8, 8];
+        private static List<Figure> blacks;
+       
         private abstract class Figure
         {
             public int X;
             public int Y;
             public int Name;
             public List<int[]> possiblePaths;
+            public PictureBox pb;
 
+            public static void imageLocation(int x, int y, PictureBox pb)
+            {
+                pb.Location = new Point(5 + x * 50, 355 - y * 50);
+            }
+            public void addImage(string image)
+            {
+                pb = new PictureBox();
+                imageLocation(X, Y, pb);
+                pb.Size = new Size(40, 40);
+                pb.Image = Image.FromFile(image);
+                pb.Visible = true;
+            }
             public void Move()
             {
                 Random rnd = new Random();
                 int move, x1, y1;
                 do
                 {
-                    move = rnd.Next(0, possiblePaths.Count - 1);
+                    move = rnd.Next(0, possiblePaths.Count);
                     x1 = possiblePaths[move][0];
                     y1 = possiblePaths[move][1];
                 } while (A[x1, y1] != 0);
@@ -33,20 +50,26 @@ namespace Pvz1
                 X = x1;
                 Y = y1;
                 A[X, Y] = Name;
+                imageLocation(X, Y, pb);
+            }
+            public void pathVisibility(bool val)
+            {
+                possiblePaths.ForEach(i => pbErr[i[0], i[1]].Visible = val);
             }
             public void drawPaths(int[,] B)
             {
                 possiblePaths.ForEach(i => B[i[0], i[1]] = 9);
-            }            
+            }
             public abstract void GeneratePaths();
         }
         private class King : Figure
         {
-            public King(int x, int y, int name)
+            public King(int x, int y, int name, string image)
             {
                 X = x;
                 Y = y;
                 Name = name;
+                addImage(image);
             }
 
             public override void GeneratePaths()
@@ -64,11 +87,12 @@ namespace Pvz1
         }
         private class Bishop : Figure
         {
-            public Bishop(int x, int y, int name)
+            public Bishop(int x, int y, int name, string image)
             {
                 X = x;
                 Y = y;
                 Name = name;
+                addImage(image);
             }
 
             public override void GeneratePaths()
@@ -118,11 +142,12 @@ namespace Pvz1
         }
         private class Horse : Figure
         {
-            public Horse(int x, int y, int name)
+            public Horse(int x, int y, int name, string image)
             {
                 X = x;
                 Y = y;
                 Name = name;
+                addImage(image);
             }
 
             public override void GeneratePaths()
@@ -140,39 +165,40 @@ namespace Pvz1
         }
         private class Rook : Figure
         {
-            public Rook(int x, int y, int name)
+            public Rook(int x, int y, int name, string image)
             {
                 X = x;
                 Y = y;
                 Name = name;
+                addImage(image);
             }
             public override void GeneratePaths()
             {
                 possiblePaths = new List<int[]>();
                 if (X != 7)
                 {
-                    for (int x1 = X+1; x1 <= 7; x1++)
+                    for (int x1 = X + 1; x1 <= 7; x1++)
                     {
                         possiblePaths.Add(new int[] { x1, Y });
                     }
                 }
                 if (X != 0)
                 {
-                    for (int x1 = X-1; x1 >= 0; x1--)
+                    for (int x1 = X - 1; x1 >= 0; x1--)
                     {
                         possiblePaths.Add(new int[] { x1, Y });
                     }
                 }
                 if (Y != 7)
                 {
-                    for (int y1 = Y+1; y1 <= 7; y1++)
+                    for (int y1 = Y + 1; y1 <= 7; y1++)
                     {
                         possiblePaths.Add(new int[] { X, y1 });
                     }
                 }
                 if (Y != 0)
                 {
-                    for (int y1 = Y-1; y1 >= 0; y1--)
+                    for (int y1 = Y - 1; y1 >= 0; y1--)
                     {
                         possiblePaths.Add(new int[] { X, y1 });
                     }
@@ -189,21 +215,18 @@ namespace Pvz1
         Series z1, z2, p1;
         private void button2_Click(object sender, EventArgs e)
         {
-            ClearForm1();
             //---
-            richTextBox1.AppendText("Paleidimas\n");
-            //---
-            /*PictureBox pb = new PictureBox();
-            pb.Location = new Point(0, 0);
-            pb.Size = new Size(400, 400);
-            pb.Image = Image.FromFile(@"Images/WK.jpg");
-            pb.Visible = true;
-            chart1.Controls.Add(pb);
-            */
-            //---
-            //---
-            //---
-            //---
+            if (checkBox1.Checked) blacks.ForEach(figure => { figure.pathVisibility(false); });
+
+            Random rnd = new Random();
+            int u = rnd.Next(0, 4);
+            WriteLine("");
+            blacks[u].Move();
+            blacks[u].GeneratePaths();
+
+            int[,] B = (int[,])A.Clone();
+            blacks.ForEach(figure => { A[figure.X, figure.Y] = figure.Name; figure.drawPaths(B);});
+            if (checkBox1.Checked) blacks.ForEach(figure => { figure.pathVisibility(true); });
         }
         private void fillValues(int[,] A)
         {
@@ -240,36 +263,51 @@ namespace Pvz1
         private void Solution()
         {
             fillValues(A);
-            List<Figure> blacks = new List<Figure>();
-            blacks.Add(new King(4, 7, 1));
-            blacks.Add(new Rook(7, 7, 2));
-            blacks.Add(new Bishop(5, 7, 3));
-            blacks.Add(new Horse(1, 7, 4));
-            blacks.ForEach(figure => {A[figure.X, figure.Y] = figure.Name;});
+            blacks = new List<Figure>();
+            blacks.Add(new King(4, 7, 1, "Data/BK.jpg"));
+            blacks.Add(new Rook(7, 7, 2, "Data/BR.jpg"));
+            blacks.Add(new Bishop(5, 7, 3, "Data/BB.jpg"));
+            blacks.Add(new Horse(1, 7, 4, "Data/BH.jpg"));
+            blacks.ForEach(figure => { A[figure.X, figure.Y] = figure.Name; figure.GeneratePaths(); chart1.Controls.Add(figure.pb); });
 
-            King whiteKing = new King(4, 0, 8);
+            King whiteKing = new King(4, 0, 8, @"Data/WK.jpg");
+            chart1.Controls.Add(whiteKing.pb);
             A[whiteKing.X, whiteKing.Y] = whiteKing.Name;
 
-            printMatrix(A);
-            int u = 3;
-            blacks[u].GeneratePaths();
-
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 8; i++)
             {
-                WriteLine("");
-                blacks[u].Move();
-                blacks[u].GeneratePaths();
-
-                int[,] B = (int[,])A.Clone();
-                blacks[u].drawPaths(B);
-                printMatrix(B);
+                for (int u = 0; u < 8; u++)
+                {
+                    PictureBox pb1 = new PictureBox();
+                    pb1.Location = new Point(0, 0);
+                    Figure.imageLocation(i, u, pb1);
+                    pb1.Size = new Size(40, 40);
+                    pb1.Image = Image.FromFile("Data/checked.png");
+                    pb1.Visible = false;
+                    pbErr[i, u] = pb1;
+                    chart1.Controls.Add(pbErr[i, u]);
+                }
             }
+
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(0, 0);
+            pb.Size = new Size(400, 400);
+            pb.Image = Image.FromFile(@"Data/Board.jpg");
+            pb.Visible = true;
+            pb.BackColor = Color.Transparent;
+            chart1.Controls.Add(pb);
 
         }
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            blacks.ForEach(figure => { figure.pathVisibility(checkBox1.Checked); });
+        }
+
         private void button4_Click(object sender, EventArgs e)
         {
             ClearForm1();
