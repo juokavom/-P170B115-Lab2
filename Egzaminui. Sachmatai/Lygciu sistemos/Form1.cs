9bed745c3a89e73937c7b4cc6aea461deab9953a
Jokūbas Akramas <j.akramas@gmail.com>
2020-12-21 20:42:46 +0200

    feat-BFS/Paskirstymo matricos sakiciavimas priklausant, ar galima kirst juodus, ar ne

--- Egzaminui. Sachmatai/Lygciu sistemos/Form1.cs
+++ Egzaminui. Sachmatai/Lygciu sistemos/Form1.cs
@@ -32,6 +32,28 @@ namespace Pvz1
             button3.Enabled = false;
             checkBox1.Enabled = false;
         }
+        private class Cell
+        {
+            public char Symbol { get; set; }
+            public bool Visited { get; set; }
+            public bool Valid { get; set; }
+            public int[] Parent { get; set; }
+
+            public Cell(bool valid)
+            {
+                Symbol = '*';
+                Valid = valid;
+                Visited = false;
+            }
+
+            public bool Visit(int[] parent)
+            {
+                if (Visited || !Valid) return false;
+                Parent = parent;
+                Visited = true;
+                return true;
+            }
+        }
         private abstract class Figure
         {
             public int X;
@@ -77,6 +99,11 @@ namespace Pvz1
         }
         private class King : Figure
         {
+            public King(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
             public King(int x, int y, int name, string image)
             {
                 X = x;
@@ -375,12 +402,183 @@ namespace Pvz1
             //whiteKingPaths.ForEach(i => richTextBox1.AppendText(string.Format("Possible: {0} {1}, direction: {2}\n", i[0], i[1], i[2])));
             //richTextBox1.AppendText("\n");
         }
+        private static int[][] BFS(Cell[,] A, int[] start, int[] end, ref bool success)
+        {
+            int X_min = 0;
+            int X_max = 7;
+            int Y_min = 0;
+            int Y_max = 7;
+
+            int currentIndex = 0;
+            int endIndex = 1;
+
+            int[][] visited = new int[A.GetLength(0) * A.GetLength(1)][];
+            visited[0] = start;
+            A[start[0], start[1]].Symbol = '-';
+            A[start[0], start[1]].Visited = true;
+            A[start[0], start[1]].Parent = new int[] { -1, -1 };
+
+            while (true)
+            {
+                bool changed = false;
+                int x = visited[currentIndex][0];
+                int y = visited[currentIndex][1];
+
+                if (x > X_min && x <= X_max)
+                {
+                    // # # #
+                    // x o #
+                    // # # #
+                    if (A[x - 1, y].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x - 1, y };
+                        changed = true;
+                    }
+                }
+                if (x < X_max && x >= X_min)
+                {
+                    // # # #
+                    // # o x
+                    // # # #
+                    if (A[x + 1, y].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x + 1, y };
+                        changed = true;
+                    }
+                }
+                if (y > Y_min && y <= Y_max)
+                {
+                    // # # #
+                    // # o #
+                    // # x #
+                    if (A[x, y - 1].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x, y - 1 };
+                        changed = true;
+                    }
+                }
+                if (y < Y_max && y >= Y_min)
+                {
+                    // # x #
+                    // # o #
+                    // # # #
+                    if (A[x, y + 1].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x, y + 1 };
+                        changed = true;
+                    }
+                }
+                if (x > X_min && x <= X_max && y > Y_min && y <= Y_max)
+                {
+                    // # # #
+                    // # o #
+                    // x # #
+                    if (A[x - 1, y - 1].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x - 1, y - 1 };
+                        changed = true;
+                    }
+                }
+                if (x > X_min && x <= X_max && y < Y_max && y >= Y_min)
+                {
+                    // x # #
+                    // # o #
+                    // # # #
+                    if (A[x - 1, y + 1].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x - 1, y + 1 };
+                        changed = true;
+                    }
+                }
+                if (x < X_max && x >= X_min && y > Y_min && y <= Y_max)
+                {
+                    // # # #
+                    // # o #
+                    // # # x
+                    if (A[x + 1, y - 1].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x + 1, y - 1 };
+                        changed = true;
+                    }
+                }
+                if (x < X_max && x >= X_min && y < Y_max && y >= Y_min)
+                {
+                    // # # x
+                    // # o #
+                    // # # #
+                    if (A[x + 1, y + 1].Visit(visited[currentIndex]))
+                    {
+                        visited[endIndex++] = new int[] { x + 1, y + 1 };
+                        changed = true;
+                    }
+                }
+                if (currentIndex < visited.GetLength(0)) currentIndex++;
+                if (A[end[0], end[1]].Visited) { success = true; break; }
+                if (!changed) { success = false; break; }
+            }
+            List<int[]> list = new List<int[]>();
+            list.Add(new int[] { end[0], end[1] });
+            for (Cell curr = A[end[0], end[1]]; curr.Parent[0] != -1; curr = A[curr.Parent[0], curr.Parent[1]])
+            {
+                curr.Symbol = '-';
+                list.Add(new int[] { curr.Parent[0], curr.Parent[1] });
+            }
+            list.Reverse();
+            return list.ToArray();
+
+        }
+        private int BFSOLUTION()
+        {/*
+            int sum = 0;
+            for (int i = 0; i < 7; i++) 
+            {
+                if(A[i, 7] == )
+            }*/
+            return 0;
+        }
+        private Cell[,] spreadMatrix()
+        {
+            Cell[,] B = new Cell[8, 8];
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int u = 0; u < B.GetLength(1); u++)
+                {
+                    B[i, u] = new Cell(true);
+                    bool condition = false;
+                    if (radioButton2.Checked) PATHS.ForEach(q => { if (q[0] == i && q[1] == u) condition = true; });
+                    else if (radioButton1.Checked) PATHS.ForEach(q => { if (q[0] == i && q[1] == u) { if (A[i, u] == 1 || q[2] == 1) condition = true; }; });
+                    if (condition)
+                    {
+                        B[i, u].Valid = false; B[i, u].Symbol = 'o';
+                    }
+                }
+            }
+            return B;
+        }
+        private void printMatrix(Cell[,] array)
+        {
+            string line = new string('-', array.GetLength(0) * 2 - 1);
+            System.Diagnostics.Debug.WriteLine(line);
+            for (int y = array.GetLength(1) - 1; y >= 0; y--)
+            {
+                for (int x = 0; x < array.GetLength(0); x++)
+                {
+                    System.Diagnostics.Debug.Write(string.Format("{0} ", array[x, y].Symbol));
+                }
+                System.Diagnostics.Debug.WriteLine("");
+            }
+            System.Diagnostics.Debug.WriteLine(line);
+        }
         private bool whiteKingMove()
         {
-            int selectedPath = 0;
+            int selectedPath = BFSOLUTION();
+            Cell[,] SM = spreadMatrix();
+            //
+            //BFS (jei ne, tai ims zemesnius)
+            //
             bool left = (whiteKing.X > 3) ? true : false;
             //I virsu
-            whiteKingPaths.ForEach(i => { if (i[2] == 8) selectedPath = i[2]; }); // Jei i virsu
+            if (selectedPath == 0) whiteKingPaths.ForEach(i => { if (i[2] == 8) selectedPath = i[2]; }); // Jei i virsu
             if (left)
             {
                 if (selectedPath == 0) whiteKingPaths.ForEach(i => { if (i[2] == 7) selectedPath = i[2]; }); //Jei i virsu ir kaire
@@ -439,7 +637,7 @@ namespace Pvz1
         {
             paths.ForEach(i => pbErr[i[0], i[1]].Visible = val);
         }
-              
+
         //KITI METODAi
         private void fillValues(int[,] A)
         {
@@ -471,7 +669,7 @@ namespace Pvz1
         private void WriteLine(string text)
         {
             Write(text + "\n");
-        }       
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
