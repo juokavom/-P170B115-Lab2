f392d3754b7fb693b6ff992ee76c7d6380082004
Jokubas Akramas <j.akramas@gmail.com>
2021-03-03 14:23:07 +0200

    Pridetas primas laboratorinis

---
+++ Egzaminui. Figuros/README.txt
@@ -0,0 +1,1 @@
+neatlikta iki galo. 
---
+++ Netiesines lygtys/Pvz1/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace Pvz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.stygu = new System.Windows.Forms.RadioButton();
+            this.iteraciju = new System.Windows.Forms.RadioButton();
+            this.skenavimo = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(669, 394);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 412);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(669, 251);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(773, 643);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Baigti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // 
+            // 
+            this.stygu.Location = new System.Drawing.Point(692, 10);
+            this.stygu.Text = "1.Stygų m.";
+            this.iteraciju.Location = new System.Drawing.Point(692, 50);
+            this.iteraciju.Text = "2.Paprastųjų it.";
+            this.skenavimo.Location = new System.Drawing.Point(692, 90);
+            this.skenavimo.Text = "5.Skenavimo maž.";
+            // 
+            //
+            //
+            this.button2.Location = new System.Drawing.Point(692, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "f(x) daugianaris";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button3.Location = new System.Drawing.Point(692, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "g(x) transcendentinė funkcija";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // button2
+            // 
+            this.button5.Location = new System.Drawing.Point(692, 300);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(156, 23);
+            this.button3.TabIndex = 5;
+            this.button5.Text = "Antra užduotis";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(692, 643);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Valyti";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 678);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.stygu);
+            this.Controls.Add(this.iteraciju);
+            this.Controls.Add(this.skenavimo);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Lab1";
+            this.Text = "Skaitiniai metodai ir Algoritmai";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        /// <summary>
+        /// Inicializacijos veiksmai
+        /// </summary>
+        private void Initialize()
+        {
+            // pridedam timerius
+            Timerlist.Clear();
+            Timerlist.Add(timer1);
+            Timerlist.Add(timer2);
+        }
+
+        /// <summary>
+        /// Paruošiamas langas vaizdavimui
+        /// </summary>
+        public void PreparareForm(float xmin, float xmax, float ymin, float ymax)
+        {
+
+            float x_grids = 10;
+            //double xmin = 0; double xmax = 2 * Math.PI;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.MajorTickMark.Interval = (xmax - xmin) / x_grids;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Font = new Font("Courier New", 8, FontStyle.Bold);
+
+            float y_grids = 10;
+            //double ymin = -1; double ymax = 1;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.MajorTickMark.Interval = (ymax - ymin) / y_grids;
+            chart1.ChartAreas[0].AxisY.LabelStyle.Font = new Font("Courier New", 8, FontStyle.Bold);
+
+            chart1.ChartAreas[0].AxisX.Minimum = xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = xmax;
+            chart1.ChartAreas[0].AxisY.Minimum = ymin;
+            chart1.ChartAreas[0].AxisY.Maximum = ymax;
+
+            chart1.Legends[0].Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorX.Interval = 0.01;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorY.Interval = 0.01;
+            
+        }
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Button button2;
+        private RadioButton stygu;
+        private RadioButton iteraciju;
+        private RadioButton skenavimo;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+
+
+
+}
+
